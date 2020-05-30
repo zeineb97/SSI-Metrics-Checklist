47adb2b30cd4a20842f7a3681f06ecf678ff40aa
zeineb chabchoub <zeinebchabchoub97@gmail.com>
2020-05-30 23:46:23 +0100

    update the data model

--- src/server.js
+++ src/server.js
@@ -69,34 +69,29 @@ const createPhase = function(phase) {
       { new: true, useFindAndModify: false }
     );
   };
-  
+
+  const addSousTacheToPhase = function(sousTacheId, phaseId) {
+    return db.sous_tache.findByIdAndUpdate(
+        sousTacheId,
+      { phase_id: phaseId },
+      { new: true, useFindAndModify: false }
+    );
+  };
+
+
+  const addSousTacheToExigence = function(sousTacheId, exigenceId) {
+    return db.sous_tache.findByIdAndUpdate(
+        sousTacheId,
+      { exigence_id: exigenceId },
+      { new: true, useFindAndModify: false }
+    );
+  };
+
+
   const getSousTacheWithPopulate = function(id) {
     return db.sous_tache.findById(id)
       .populate("sous_tache", "name -_id")
   };
-
-  
-  // supplémentaire !! 
-  
-  const getExigencesInPhase = function(phaseId) {
-    return db.phase.find({ phase: phaseId })
-      .populate("phase", "name -_id")
-
-  };
-  const getTachesInExogence = function(phaseId) {
-    return db.phase.find({ phase: phaseId })
-      .populate("exigence", "name -_id")
-
-  };
-
-  const getSousTachesInTache = function(tacheId) {
-    return db.phase.find({ tache: tacheId })
-      .populate("tache", "name -_id")
-
-  };
-
-  
-
 
 
   const run = async function() {
@@ -279,7 +274,7 @@ var tache9= await createTache({
 tache9 =  await addTacheToExigence(tache9._id, exigence9._id);
 tache9 = await getTacheWithPopulate(tache9._id);
 
-// Insert9ion 10
+// Insertion 10
 
 var tache10= await createTache({
   label : "Management review",
@@ -315,6 +310,8 @@ var sstache1= await createSousTache({
    label : "Déterminer les problèmes externes et internes qui sont pertinents pour  l'objectifde l'organisation et qui affectent sa capacité à atteindre les résultats attendues de son SMSI."
 });
 sstache1 =  await addSousTacheToTache(sstache1._id, tache1._id);
+sstache1 =  await addSousTacheToExigence(sstache1._id, exigence4._id);
+sstache1 =  await addSousTacheToPhase(sstache1._id, phase1._id);
 sstache1 = await getSousTacheWithPopulate(sstache1._id);
 
 // insertion 2 
@@ -326,6 +323,8 @@ var sstache2= await createSousTache({
    label : "Déterminer les parties intéressées pertinentes pour le  SMSI"
 });
 sstache2 =  await addSousTacheToTache(sstache2._id, tache2._id);
+sstache2 =  await addSousTacheToExigence(sstache2._id, exigence4._id);
+sstache2 =  await addSousTacheToPhase(sstache2._id, phase1._id);
 sstache2 = await getSousTacheWithPopulate(sstache2._id);
 // insertion 3 
 var sstache3= await createSousTache({
@@ -336,6 +335,8 @@ var sstache3= await createSousTache({
         label : "Déterminer les exigences de ces parties intéressées concernant la sécurité de l'information"
 });
 sstache3 =  await addSousTacheToTache(sstache3._id, tache2._id);
+sstache3 =  await addSousTacheToExigence(sstache3._id, exigence4._id);
+sstache3 =  await addSousTacheToPhase(sstache3._id, phase1._id);
 sstache3 = await getSousTacheWithPopulate(sstache3._id);
 // insertion 4 
 var sstache4= await createSousTache({
@@ -346,6 +347,8 @@ var sstache4= await createSousTache({
         label : "Déterminer les limites et l'applicabilité du SMSI "
 });
 sstache4 =  await addSousTacheToTache(sstache4._id, tache3._id);
+sstache4 =  await addSousTacheToExigence(sstache4._id, exigence4._id);
+sstache4 =  await addSousTacheToPhase(sstache4._id, phase1._id);
 sstache4 = await getSousTacheWithPopulate(sstache4._id);
 // insertion 5 
 var sstache5= await createSousTache({
@@ -356,6 +359,8 @@ var sstache5= await createSousTache({
         label : "Planifier des actions pour faire face à ces risques et opportunités"
 });
 sstache5 =  await addSousTacheToTache(sstache5._id, tache4._id);
+sstache5 =  await addSousTacheToExigence(sstache5._id, exigence6._id);
+sstache5 =  await addSousTacheToPhase(sstache5._id, phase1._id);
 sstache5 = await getSousTacheWithPopulate(sstache5._id);
 // insertion 6 
 var sstache6= await createSousTache({
@@ -366,6 +371,8 @@ var sstache6= await createSousTache({
         label : "Planifier comment intégrer et mettre en œuvre ces actions dans son SMSI"
 });
 sstache6 =  await addSousTacheToTache(sstache6._id, tache4._id);
+sstache6 =  await addSousTacheToExigence(sstache6._id, exigence6._id);
+sstache6 =  await addSousTacheToPhase(sstache6._id, phase1._id);
 sstache6 = await getSousTacheWithPopulate(sstache6._id);
 
 // insertion 7 
@@ -377,6 +384,8 @@ var sstache7= await createSousTache({
         label : "Planifier comment  évaluer l'efficacité de ces actions"
 });
 sstache7 =  await addSousTacheToTache(sstache7._id, tache4._id);
+sstache7 =  await addSousTacheToExigence(sstache7._id, exigence6._id);
+sstache7 =  await addSousTacheToPhase(sstache7._id, phase1._id);
 sstache7 = await getSousTacheWithPopulate(sstache7._id);
 
 
@@ -389,6 +398,8 @@ var sstache8= await createSousTache({
    label : "Examen de l'exactitude des risques selon l'évalution du RSSI"
 });
 sstache8 =  await addSousTacheToTache(sstache8._id, tache5._id);
+sstache8 =  await addSousTacheToExigence(sstache8._id, exigence6._id);
+sstache8 =  await addSousTacheToPhase(sstache8._id, phase1._id);
 sstache8 = await getSousTacheWithPopulate(sstache8._id);
 
 
@@ -401,6 +412,8 @@ var sstache9= await createSousTache({
         label : "Sélectionner les options appropriées de traitement des risques pour la sécurité de l'information, en tenant compte des résultats de l'évaluation des risques"
 });
 sstache9 =  await addSousTacheToTache(sstache9._id, tache6._id);
+sstache9 =  await addSousTacheToExigence(sstache9._id, exigence6._id);
+sstache9 =  await addSousTacheToPhase(sstache9._id, phase1._id);
 sstache9 = await getSousTacheWithPopulate(sstache9._id);
 
 // insertion 10 
@@ -412,6 +425,8 @@ var sstache10= await createSousTache({
         label : "Déterminer tous les contrôles nécessaires à la mise en œuvre des options de traitement des risques de sécurité de l'information choisies"
 });
 sstache10 =  await addSousTacheToTache(sstache10._id, tache6._id);
+sstache10 =  await addSousTacheToExigence(sstache10._id, exigence6._id);
+sstache10 =  await addSousTacheToPhase(sstache10._id, phase1._id);
 sstache10 = await getSousTacheWithPopulate(sstache10._id);
 
 // insertion 11 
@@ -420,9 +435,11 @@ var sstache11= await createSousTache({
        date_debut : "2022-01-10T23:00:00.000Z",
         date_fin: "2022-03-10T23:00:00.000Z",
         etat : "pas mis en oeuvre",
-        label : "Comparer les contrôles déterminés au 6.1.3 (b) ci-dessus avec ceux de l'annexe A et vérifier qu'aucun contrôle nécessaire n'a été omis"
+        label : "Comparer les contrôles déterminés au 6.1.3.2 ci-dessus avec ceux de l'annexe A et vérifier qu'aucun contrôle nécessaire n'a été omis"
 });
 sstache11 =  await addSousTacheToTache(sstache11._id, tache6._id);
+sstache11 =  await addSousTacheToExigence(sstache11._id, exigence6._id);
+sstache11 =  await addSousTacheToPhase(sstache11._id, phase1._id);
 sstache11 = await getSousTacheWithPopulate(sstache11._id);
 // insertion 12 
 var sstache12= await createSousTache({
@@ -430,9 +447,11 @@ var sstache12= await createSousTache({
   date_debut : "2022-01-11T23:00:00.000Z",
    date_fin: "2022-03-11T23:00:00.000Z",
    etat : "pas mis en oeuvre",
-   label : "Produire une déclaration d'applicabilité contenant les contrôles nécessaires (voir 6.1.3.b et c) et la justification des inclusions, qu'elles soient mises en œuvre ou non, et la justification des exclusions des contrôles de l'annexe A"
+   label : "Produire une déclaration d'applicabilité contenant les contrôles nécessaires (voir 6.1.3.2 et 6.1.3.3) et la justification des inclusions, qu'elles soient mises en œuvre ou non, et la justification des exclusions des contrôles de l'annexe A"
 });
 sstache12 =  await addSousTacheToTache(sstache12._id, tache6._id);
+sstache12 =  await addSousTacheToExigence(sstache12._id, exigence6._id);
+sstache12 =  await addSousTacheToPhase(sstache12._id, phase1._id);
 sstache12 = await getSousTacheWithPopulate(sstache12._id);
 // insertion 13 
 var sstache13= await createSousTache({
@@ -443,6 +462,8 @@ var sstache13= await createSousTache({
         label : "Formuler un plan de traitement des risques de sécurité de l'information"
 });
 sstache13 =  await addSousTacheToTache(sstache13._id, tache6._id);
+sstache13 =  await addSousTacheToExigence(sstache13._id, exigence6._id);
+sstache13 =  await addSousTacheToPhase(sstache13._id, phase1._id);
 sstache13 = await getSousTacheWithPopulate(sstache13._id);
 // insertion 14 
 var sstache14= await createSousTache({
@@ -453,6 +474,8 @@ var sstache14= await createSousTache({
    label : "Obtenir l'approbation des propriétaires de risques du plan de traitement des risques de sécurité de l'information et l'acceptation des risques résiduels de sécurité de l'information"
 });
 sstache14 =  await addSousTacheToTache(sstache14._id, tache6._id);
+sstache14 =  await addSousTacheToExigence(sstache14._id, exigence6._id);
+sstache14 =  await addSousTacheToPhase(sstache14._id, phase1._id);
 sstache14 = await getSousTacheWithPopulate(sstache14._id);
 
 
@@ -465,6 +488,8 @@ var sstache15= await createSousTache({
    label : "Déterminer sur quoi communiquer"
 });
 sstache15 =  await addSousTacheToTache(sstache15._id, tache7._id);
+sstache15 =  await addSousTacheToExigence(sstache15._id, exigence7._id);
+sstache15 =  await addSousTacheToPhase(sstache15._id, phase2._id);
 sstache15 = await getSousTacheWithPopulate(sstache15._id);
 // insertion 16 
 var sstache16= await createSousTache({
@@ -475,6 +500,8 @@ var sstache16= await createSousTache({
    label : "Déterminer quand communiquer"
 });
 sstache16 =  await addSousTacheToTache(sstache16._id, tache7._id);
+sstache16 =  await addSousTacheToExigence(sstache16._id, exigence7._id);
+sstache16 =  await addSousTacheToPhase(sstache16._id, phase2._id);
 sstache16 = await getSousTacheWithPopulate(sstache16._id);
 // insertion 17 
 var sstache17= await createSousTache({
@@ -485,6 +512,8 @@ var sstache17= await createSousTache({
         label : " Déterminer avec qui communiquer"
 });
 sstache17 =  await addSousTacheToTache(sstache17._id, tache7._id);
+sstache17 =  await addSousTacheToExigence(sstache17._id, exigence7._id);
+sstache17 =  await addSousTacheToPhase(sstache17._id, phase2._id);
 sstache17 = await getSousTacheWithPopulate(sstache17._id);
 // insertion 18 
 var sstache18= await createSousTache({
@@ -495,6 +524,8 @@ var sstache18= await createSousTache({
         label : "Déterminer qui doit communiquer"
 });
 sstache18 =  await addSousTacheToTache(sstache18._id, tache7._id);
+sstache18 =  await addSousTacheToExigence(sstache18._id, exigence7._id);
+sstache18 =  await addSousTacheToPhase(sstache18._id, phase2._id);
 sstache18 = await getSousTacheWithPopulate(sstache18._id);
 // insertion 19 
 var sstache19= await createSousTache({
@@ -505,6 +536,8 @@ var sstache19= await createSousTache({
         label : "Déterminer les processus par lesquels la communication doit être effectuée."
 });
 sstache19 =  await addSousTacheToTache(sstache19._id, tache7._id);
+sstache19 =  await addSousTacheToExigence(sstache19._id, exigence7._id);
+sstache19 =  await addSousTacheToPhase(sstache19._id, phase2._id);
 sstache19 = await getSousTacheWithPopulate(sstache19._id);
 
 
@@ -517,6 +550,8 @@ var sstache20= await createSousTache({
    label : "Déterminer ce qui doit être surveillé et mesuré, y compris les processus et contrôles de sécurité de l'information;"
 });
 sstache20 =  await addSousTacheToTache(sstache20._id, tache8._id);
+sstache20 =  await addSousTacheToExigence(sstache20._id, exigence9._id);
+sstache20 =  await addSousTacheToPhase(sstache20._id, phase3._id);
 sstache20 = await getSousTacheWithPopulate(sstache20._id);
 // insertion 21 
 var sstache21= await createSousTache({
@@ -527,6 +562,8 @@ var sstache21= await createSousTache({
         label : "Déterminer les méthodes de surveillance, de mesure, d'analyse et d'évaluation, selon le cas, pour garantir des résultats valides"
 });
 sstache21 =  await addSousTacheToTache(sstache21._id, tache8._id);
+sstache21 =  await addSousTacheToExigence(sstache21._id, exigence9._id);
+sstache21 =  await addSousTacheToPhase(sstache21._id, phase3._id);
 sstache21 = await getSousTacheWithPopulate(sstache21._id);
 // insertion 22 
 var sstache22= await createSousTache({
@@ -537,6 +574,8 @@ var sstache22= await createSousTache({
    label : "Déterminer quand le contrôle et la mesure doivent être effectués"
 });
 sstache22 =  await addSousTacheToTache(sstache22._id, tache8._id);
+sstache22 =  await addSousTacheToExigence(sstache22._id, exigence9._id);
+sstache22 =  await addSousTacheToPhase(sstache22._id, phase3._id);
 sstache22 = await getSousTacheWithPopulate(sstache22._id);
 
 // insertion 23 
@@ -548,6 +587,8 @@ var sstache23= await createSousTache({
    label : "Déterminer qui doit surveiller et mesurer"
 });
 sstache23 =  await addSousTacheToTache(sstache23._id, tache8._id);
+sstache23 =  await addSousTacheToExigence(sstache23._id, exigence9._id);
+sstache23 =  await addSousTacheToPhase(sstache23._id, phase3._id);
 sstache23 = await getSousTacheWithPopulate(sstache23._id);
 
 // insertion 24 
@@ -559,6 +600,8 @@ var sstache24= await createSousTache({
         label : "Déterminer quand les résultats de la surveillance et de la mesure doivent être analysés et évalués"
 });
 sstache24 =  await addSousTacheToTache(sstache24._id, tache8._id);
+sstache24 =  await addSousTacheToExigence(sstache24._id, exigence9._id);
+sstache24 =  await addSousTacheToPhase(sstache24._id, phase3._id);
 sstache24 = await getSousTacheWithPopulate(sstache24._id);
 
 // insertion 25 
@@ -570,6 +613,8 @@ var sstache25= await createSousTache({
         label : "Déterminer qui analysera et évaluera ces résultats"
 });
 sstache25 =  await addSousTacheToTache(sstache25._id, tache8._id);
+sstache25 =  await addSousTacheToExigence(sstache25._id, exigence9._id);
+sstache25 =  await addSousTacheToPhase(sstache25._id, phase3._id);
 sstache25 = await getSousTacheWithPopulate(sstache25._id);
 
 // insertion 26 
@@ -581,6 +626,8 @@ var sstache26= await createSousTache({
    label : "Audit interne"
 });
 sstache26 =  await addSousTacheToTache(sstache26._id, tache9._id);
+sstache26 =  await addSousTacheToExigence(sstache26._id, exigence9._id);
+sstache26 =  await addSousTacheToPhase(sstache26._id, phase3._id);
 sstache26 = await getSousTacheWithPopulate(sstache26._id);
 
 // insertion 27 
@@ -592,6 +639,8 @@ var sstache27= await createSousTache({
    label : "Management review"
 });
 sstache27 =  await addSousTacheToTache(sstache27._id, tache10._id);
+sstache27 =  await addSousTacheToExigence(sstache27._id, exigence9._id);
+sstache27 =  await addSousTacheToPhase(sstache27._id, phase3._id);
 sstache27 = await getSousTacheWithPopulate(sstache27._id);
 
 // insertion 28 
@@ -603,6 +652,8 @@ var sstache28= await createSousTache({
         label : "Examiner la non-conformité"
 });
 sstache28 =  await addSousTacheToTache(sstache28._id, tache11._id);
+sstache28 =  await addSousTacheToExigence(sstache28._id, exigence10._id);
+sstache28 =  await addSousTacheToPhase(sstache28._id, phase4._id);
 sstache28 = await getSousTacheWithPopulate(sstache28._id);
 
 // insertion 29 
@@ -614,6 +665,8 @@ var sstache29= await createSousTache({
         label : "Déterminer les causes de la non-conformité"
 });
 sstache29 =  await addSousTacheToTache(sstache29._id, tache11._id);
+sstache29 =  await addSousTacheToExigence(sstache29._id, exigence10._id);
+sstache29 =  await addSousTacheToPhase(sstache29._id, phase4._id);
 sstache29 = await getSousTacheWithPopulate(sstache29._id);
 
 // insertion 30 
@@ -625,6 +678,8 @@ var sstache30= await createSousTache({
         label : "Déterminer si des non-conformités similaires existent ou pourraient se produire"
 });
 sstache30 =  await addSousTacheToTache(sstache30._id, tache11._id);
+sstache30 =  await addSousTacheToExigence(sstache30._id, exigence10._id);
+sstache30 =  await addSousTacheToPhase(sstache30._id, phase4._id);
 sstache30 = await getSousTacheWithPopulate(sstache30._id);
 
 // insertion 31 
@@ -636,6 +691,8 @@ var sstache31= await createSousTache({
    label : "Examiner l'efficacité de toute mesure corrective prise"
 });
 sstache31 =  await addSousTacheToTache(sstache31._id, tache11._id);
+sstache31 =  await addSousTacheToExigence(sstache31._id, exigence10._id);
+sstache31 =  await addSousTacheToPhase(sstache31._id, phase4._id);
 sstache31 = await getSousTacheWithPopulate(sstache31._id);
 
 // insertion 32 
@@ -647,6 +704,8 @@ var sstache32= await createSousTache({
         label : "Apporter des modifications au SMSI"
 });
 sstache32 =  await addSousTacheToTache(sstache32._id, tache11._id);
+sstache32 =  await addSousTacheToExigence(sstache32._id, exigence10._id);
+sstache32 =  await addSousTacheToPhase(sstache32._id, phase4._id);
 sstache32 = await getSousTacheWithPopulate(sstache32._id);
 
 console.log("Kamelna !!! ");
